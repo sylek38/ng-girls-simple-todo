9f455b3c6c5fb181eafe881990ef5dcb8307a97c
sylek38 <sylek38@gmail.com>
2021-05-11 20:40:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng-girls-simple-todo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-girls-simple-todo)
---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  templateUrl: './input-button-unit.component.html',
+  styleUrls: ['./input-button-unit.component.css']
+})
+export class InputButtonUnitComponent implements OnInit {
+  title: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  changeTitle(newTitle: string) {
+  this.title = newTitle;
+}
+
+}
